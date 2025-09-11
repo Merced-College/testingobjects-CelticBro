26eab3a9569f31fac628801860d10d215a01acdf
Ashton L. <104606068+SwampMud@users.noreply.github.com>
2025-09-11 10:16:16 -0700

    Update ObjectTester.java
    Book testing as well

--- ObjectTester.java
+++ ObjectTester.java
@@ -13,6 +13,21 @@ public class ObjectTester {
 
     //only print dog name
     System.out.println(dog2.getName());
+
+    //Book testing - default book
+   Book book1 = new Book();
+   System.out.println(book1);
+
+   //non default book 
+   Book book2 = new Book ("American Gods", "Neil Gaimen", "4/5")
+   System.out.println(book2)
+
+   book2.setScore("5/5");
+   System.out.println(book2);
+
+   //print specific parts of the whole
+   System.out.println(book2.getAuthor());
+   
     }//end main
 
 }//end class
